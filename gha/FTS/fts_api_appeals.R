0faec64845808c4bf51e8307532b143144a0af7f
danjwalton <20973540+danjwalton@users.noreply.github.com>
2021-10-25 10:46:09 +0100

    Add FTS API function

---
+++ gha/FTS/fts_api_appeals.R
@@ -0,0 +1,27 @@
+#Description:  Retrieve aggregate totals from UN OCHA's FTS API
+#Author: Dan W
+#Creation date: Oct 2021
+#Last revision:
+#Notes: 
+
+fts_get_appeals <- function(years=NULL){
+  
+  required.packages <- c("data.table","jsonlite")
+  lapply(required.packages, require, character.only=T)
+  
+  base.url <- "https://api.hpc.tools/v1/public/plan/year/"
+  
+  plans.list <- list()
+  if(length(years > 1)){
+    for(i in 1:length(years)){
+      url <- paste0(base.url, years[i])
+      plans <- fromJSON(url)$data
+      plans.list[[i]] <- plans
+    }
+    
+    plans.list <- rbindlist(plans.list)
+  } else {
+    plans.list <- fromJSON(base.url)$data
+  }
+  return(plans.list)
+}
