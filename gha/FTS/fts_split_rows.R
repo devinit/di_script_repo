5df2d1a08667ac56f9789c9179c2c5e5fc777c08
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-04-13 12:27:21 +0100

    Update fts_split_rows.R

--- gha/FTS/fts_split_rows.R
+++ gha/FTS/fts_split_rows.R
@@ -6,7 +6,7 @@
 
 fts_split_rows <- function(data, value.cols = "amountUSD", split.col = "destinationObjects_Location.name", split.pattern = "; ", remove.unsplit = T){
   split.pattern <- trimws(split.pattern)
-  temp <- data[, .(trimws(unlist(strsplit(get(split.col), split.pattern))), get(value.cols)/(1+ifelse(is.na(get(split.col)), 0, nchar(get(split.col))-nchar(gsub(split.pattern, "", get(split.col)))))), by=rownames(data)]
+  temp <- data[, .(trimws(unlist(strsplit(get(split.col), split.pattern))), as.numeric(get(value.cols))/(1+ifelse(is.na(get(split.col)), 0, nchar(get(split.col))-nchar(gsub(split.pattern, "", get(split.col)))))), by=rownames(data)]
   if(remove.unsplit){
     names(temp) <- c("rownames", split.col, value.cols)
     data[, (split.col) := NULL]
